50ec139de7d7daba79637f4cad871ae989411e45
courtneytern <courtneytern@users.noreply.github.com>
2020-01-29 12:18:07 -0500

    updated
    updated parse files and getfastq

--- biosampleresults/getfastq.sh
+++ biosampleresults/getfastq.sh
@@ -5,34 +5,34 @@
 #
 #SBATCH -J simCline # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
-#SBATCH -N 1167
+#SBATCH -N 1
 #SBATCH -t 6:00:00 ### 6 hours
-#SBATCH --mem 28G
+#SBATCH --mem 1G
 #SBATCH -o /scratch/cat7ep/fastq/simCline.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/fastq/simCline.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
-#
-#SBATCH --array=1-1167 ###check true number
 
-srun ./getfastq.sh $SLURM_ARRAY_TASK_ID
+#####srun ./getfastq.sh $SLURM_ARRAY_TASK_ID
 
 ##############################
 ##In rivanna, un-comment below
 module load sratoolkit/2.9.1
 
+#SLURM_ARRAY_TASK_ID=4
+echo ${SLURM_ARRAY_TASK_ID}
 
 ##takes in parameter of the row number to access
-SRS=$(grep ^${SLURM_ARRAY_TASK_ID}"," concatenated.csv | \
-awk -F"," '{
-  split ($0,array,",")
-  SRSnum= array[15]
-  name= array[2]""array[6]""array[7]""array[11]
-  #print name
-  print SRSnum
-}' )
+SRS=$( grep ^${SLURM_ARRAY_TASK_ID}"," ~/simCline/biosampleresults/concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[15]
+      #name= array[2]""array[6]""array[7]""array[11]
+      #print name
+    print SRSnum
+  }' )
 
   ##only need fastq-dump command by itself in rivanna
   ##./sratoolkit.2.9.6-1-mac64/bin/fastq-dump -X 5 -Z
-  
-fastq-dump -X 5 ${SRS} > cat7ep@rivanna.hpc.virginia.edu:/scratch/cat7ep/fastq
+
+fastq-dump ${SRS} > /scratch/cat7ep/fastq/${SRS}.fastq
