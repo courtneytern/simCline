4883726af56f0d10a9daccc292a8afb2d72e6c16
courtneytern <courtneytern@users.noreply.github.com>
2020-02-04 19:01:56 -0500

    corrected bastideparse
    corrected bastideparse to create bastidecat.csv with proper row numbers added and good syntax for getfastq.sh

--- biosampleresults/getfastq.sh
+++ biosampleresults/getfastq.sh
@@ -16,13 +16,13 @@
 ##In rivanna, un-comment below
 module load sratoolkit/2.9.1
 
-#SLURM_ARRAY_TASK_ID=4
+SLURM_ARRAY_TASK_ID=4
 #sbatch --array=1-19 /scratch/cat7ep/simCline/biosampleresults/getfastq.sh
 ###array=1-1206 for total concatenated, 1-19 for bastide only
-echo ${SLURM_ARRAY_TASK_ID}
+#echo ${SLURM_ARRAY_TASK_ID}
 
 ##takes in parameter of the row number to access
-SRS=$( grep ^${SLURM_ARRAY_TASK_ID}"," ~/simCline/biosampleresults/bastideParse.csv | \
+SRS=$( grep ^${SLURM_ARRAY_TASK_ID}"," ~/simCline/biosampleresults/bastidecat.csv | \
   awk -F"," '{
     split ($0,array,",")
     SRSnum= array[15]
