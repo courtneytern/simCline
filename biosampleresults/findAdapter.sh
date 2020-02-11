8b18dd6625c7d1fc4e1a5ccfaa44b5cd6d5d09e3
courtneytern <courtneytern@users.noreply.github.com>
2020-02-10 22:36:54 -0500

    Update findAdapter.sh

--- biosampleresults/findAdapter.sh
+++ biosampleresults/findAdapter.sh
@@ -7,8 +7,9 @@ file=$1
 ## run fastq_detect.pl on given file
 ## grep the line with the version marked as the match
 ## split the line into just the name of the version
-/scratch/cat7ep/simCline/biosampleresults/fastq_detect.pl ${file} | \
-  grep 'x' | \
+/scratch/cat7ep/simCline/biosampleresults/fastq_detect.pl ${file} > temp_out.txt
+
+  grep 'x' temp_out.txt | \
   awk '{
     split($0,out," :")
     print out[1]
