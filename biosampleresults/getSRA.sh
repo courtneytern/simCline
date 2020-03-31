2f6496ecadea4c2092ba32f1ef75b991dff7d489
courtneytern <courtneytern@users.noreply.github.com>
2020-03-31 19:27:59 -0400

    Update getSRA.sh
    made into rivanna slurm script

--- biosampleresults/getSRA.sh
+++ biosampleresults/getSRA.sh
@@ -1,9 +1,26 @@
-## run through concatenated file with awk
-## for every remaining SRA number (save as variable from awk'{}' > ), copy all fastq files from fastq to project
-## $15 is row O sra $16 is row P identifier
+#!/bin/sh
+#
+#SBATCH -J simCline # A single job name for the array
+#SBATCH --ntasks-per-node=1 # one core
+#SBATCH -N 1
+#SBATCH -t 30:00 ### 30 min
+#SBATCH --mem 1G
+#SBATCH -o /scratch/cat7ep/slurmOut/simCline.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/simCline.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
 
 
-cat /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
-#SRA={ awk -F"," '{$15}' }
-SRA="ERR2864366"
+##SLURM_ARRAY_TASK_ID=4
+#sbatch --array=1-826 /scratch/cat7ep/simCline/biosampleresults/getSRA.sh
+echo ${SLURM_ARRAY_TASK_ID}
+
+##takes in parameter of the row number to access
+SRA=$( grep ^${SLURM_ARRAY_TASK_ID}"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[15]
+    print SRSnum
+  }' )
+
 cp /scratch/cat7ep/fastq/${SRA}*.fastq /project/berglandlab/courtney/simCline/fastq
