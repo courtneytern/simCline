8dc8eb1d84b66ae22b63eeb97f3c73b1b5b411bf
courtneytern <courtneytern@users.noreply.github.com>
2020-04-18 00:15:07 -0400

    Update mappingscript.sh

--- biosampleresults/mappingscript.sh
+++ biosampleresults/mappingscript.sh
@@ -17,42 +17,59 @@ outputDir="/project/berglandlab/courtney/simCline/bamfiles"
 AdapterRemoval="/project/berglandlab/courtney/adapterremoval-2.3.1/build/AdapterRemoval"
 #set current working directory to the intermediate directory
 cd ${interDir}
+paired="null"
 
 #if paired, trim and merge
 if [ -f ${inputDir}/${sra}_1.fastq ] && [ -f ${inputDir}/${sra}_2.fastq ]; then
   {
+    ${paired}="true"
+
     ${AdapterRemoval} --file1 ${inputDir}/${sra}_1.fastq --file2 ${inputDir}/${sra}_2.fastq \
                       --basename ${sra}_paired --trimns --trimqualities --collapse
+
+    #paired reads
+    bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
+            /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
+            ${interDir}/${sra}_paired.pair1.truncated | \
+    samtools view -uh -q 20 -F 0x100 | \
+    samtools sort -o ${interDir}/${sra}.pair1.sort.bam
+    samtools index ${interDir}/${sra}.pair1.sort.bam
+
+    bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
+            /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
+            ${interDir}/${sra}_paired.pair2.truncated | \
+    samtools view -uh -q 20 -F 0x100 | \
+    samtools sort -o ${interDir}/${sra}.pair2.sort.bam
+    samtools index ${interDir}/${sra}.pair2.sort.bam
+
+    bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
+            /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
+            ${interDir}/${sra}_paired.collapsed.truncated | \
+    samtools view -uh -q 20 -F 0x100 | \
+    samtools sort -o ${interDir}/${sra}.collapsed.sort.bam
+    samtools index ${interDir}/${sra}.collapsed.sort.bam
+
+    #merge collapsed and uncollapsed bam files
+      samtools merge ${interDir}/${sra}.mergedbam.bam \
+               ${interDir}/${sra}.collapsed.sort.bam \
+               ${interDir}/${sra}.pair1.sort.bam \
+               ${interDir}/${sra}.pair2.sort.bam
+      samtools index ${interDir}/${sra}.mergedbam.bam
   }
-#if unpaired, trim
+#if unpaired
 else if [ -f ${inputDir}/${sra}_1.fastq ]; then
   {
+    ${paired}="false"
     ${AdapterRemoval} --file1 ${inputDir}/${sra}_1.fastq --basename ${sra}_unpaired --trimns --trimqualities
+
+      bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
+              /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
+              ${interDir}/${sra}_unpaired.truncated | \
+      samtools view -uh -q 20 -F 0x100 | \
+      samtools sort -o ${interDir}/${sra}.unpaired.sort.bam
+      samtools index ${interDir}/${sra}.unpaired.sort.bam
   }
 
-#map to reference genome
-#paired reads
-  bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
-          /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
-          ${interDir}/${sra}_paired.collapsed.truncated | \
-  samtools view -uh -q 20 -F 0x100 | \
-  samtools sort -o ${interDir}/${sra}.paired.sort.bam
-  samtools index ${interDir}/${sra}.paired.sort.bam
-
-#unpaired reads
-#need to include more files?
-  bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
-          /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
-          ${interDir}/${sra}_unpaired.truncated | \
-  samtools view -uh -q 20 -F 0x100 | \
-  samtools sort -o ${interDir}/${sra}.unpaired.sort.bam
-  samtools index ${interDir}/${sra}.unpaired.sort.bam
-
-#merge bam files
-  samtools merge ${interDir}/${sra}.mergedbam.bam \
-           ${interDir}/${sra}.paired.sort.bam \
-           ${interDir}/${sra}.unpaired.sort.bam
-  samtools index ${interDir}/${sra}.mergedbam.bam
 
 #remove duplicates
 java -jar $EBROOTPICARD/picard.jar MarkDuplicates MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=5000 \
@@ -63,8 +80,15 @@ java -jar $EBROOTPICARD/picard.jar MarkDuplicates MAX_FILE_HANDLES_FOR_READ_ENDS
      REMOVE_DUPLICATES=true \
      CREATE_INDEX=true
 
-# #indel realignment
-# gatk HaplotypeCaller \
-#     -i ${outputDir}/${sra}.finalmap.bam \
-#     -o ${outputDir}/${sra}.removeindels.vcf \
-#     -r /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta
+# go to indel realignment script
+if ${paired}=="true"; then
+  #open paired indel realignment script
+else if ${paired}=="false"; then
+  #open unpaired indel realignment script
+else
+  echo "ERROR in paired variable"
+
+gatk HaplotypeCaller \
+    -i ${outputDir}/${sra}.finalmap.bam \
+    -o ${outputDir}/${sra}.removeindels.vcf \
+    -r /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta
