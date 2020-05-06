0f0200c4f57fbe8c51b2c5c16ee21db2d708fdce
courtneytern <courtneytern@users.noreply.github.com>
2020-05-06 11:01:49 -0400

    Create verifybam.sh
    checks which bam files were not created

---
+++ biosampleresults/verifybam.sh
@@ -0,0 +1,11 @@
+SRS=$( grep ^[1-826]"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[15]
+    print SRSnum
+  }' )
+
+if [ -f /project/berglandlab/courtney/simCline/bamfiles/*${SRS}*
+then {}
+else
+then {echo ${SRS}}
