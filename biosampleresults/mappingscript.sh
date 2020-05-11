b90b72cd16a51e36d73e51cc7404c067e7198909
courtneytern <courtneytern@users.noreply.github.com>
2020-05-11 14:16:07 -0400

    Update mappingscript.sh
    edit slurm settings and general syntax cleaning

--- biosampleresults/mappingscript.sh
+++ biosampleresults/mappingscript.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
 #SBATCH -J simCline # A single job name for the array
-#SBATCH --ntasks-per-node=1 # one core
+#SBATCH --ntasks-per-node=20 # twenty cores
 #SBATCH -N 1
-#SBATCH -t 6:00:00 ### 6 hr
-#SBATCH --mem 1G
+#SBATCH -t 24:00:00 ### 24 hr
+#SBATCH --mem 10G
 #SBATCH -o /scratch/cat7ep/slurmOut/simCline.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/slurmOut/simCline.%A_%a.err # Standard error
 #SBATCH -p standard
@@ -19,27 +19,27 @@ module load samtools/1.10
 module load picard/2.20.6
 module load gatk/4.0.0.0
 
-echo ${SLURM_ARRAY_TASK_ID}
+echo "${SLURM_ARRAY_TASK_ID}"
 
 # define some parameters. Take in SRA accession number and unique identifier
 #test SRR2396839_1.fastq SRR2396839_2.fastq paired
 #### ./mappingscript.sh SRR2396839 TESTPAIR
 #test SRS3924975_1.fastq unpaired
-sra=$( grep ^${SLURM_ARRAY_TASK_ID}"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+sra=$( grep ^"${SLURM_ARRAY_TASK_ID}""," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
   awk -F"," '{
     split ($0,array,",")
     SRSnum= array[15]
     print SRSnum
   }' )
-identifier=$( grep ^${SLURM_ARRAY_TASK_ID}"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+identifier=$( grep ^"${SLURM_ARRAY_TASK_ID}""," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
   awk -F"," '{
     split ($0,array,",")
     id= array[16]
     print id
   }' )
 
-echo "SRA= "${sra}
-echo "Identifier= "${identifier}
+echo "SRA= ""${sra}"
+echo "Identifier= ""${identifier}"
 
 #Create directories
 inputDir="/project/berglandlab/courtney/simCline/fastq"
@@ -53,36 +53,36 @@ cd ${interDir}
 paired="null"
 echo "changed directory"
 #if paired, trim and merge
-if [ -f ${inputDir}/${sra}_1.fastq ] && [ -f ${inputDir}/${sra}_2.fastq ]
+if [ -f ${inputDir}/"${sra}"_1.fastq ] && [ -f ${inputDir}/"${sra}"_2.fastq ]
 then
   {
     paired="true"
     echo ${paired}
 
-    ${AdapterRemoval} --file1 ${inputDir}/${sra}_1.fastq --file2 ${inputDir}/${sra}_2.fastq \
-                      --basename ${sra}_paired --trimns --trimqualities --collapse
+    ${AdapterRemoval} --file1 ${inputDir}/"${sra}"_1.fastq --file2 ${inputDir}/"${sra}"_2.fastq \
+                      --basename "${sra}"_paired --trimns --trimqualities --collapse
 
     #paired reads
     bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
             /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
-            ${interDir}/${sra}_paired.pair1.truncated \
-            ${interDir}/${sra}_paired.pair2.truncated | \
+            ${interDir}/"${sra}"_paired.pair1.truncated \
+            ${interDir}/"${sra}"_paired.pair2.truncated | \
     samtools view -uh -q 20 -F 0x100 | \
-    samtools sort -o ${interDir}/${sra}.pairs.sort.bam
-    samtools index ${interDir}/${sra}.pairs.sort.bam
+    samtools sort -o ${interDir}/"${sra}".pairs.sort.bam
+    samtools index ${interDir}/"${sra}".pairs.sort.bam
 
     bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
             /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
-            ${interDir}/${sra}_paired.collapsed.truncated | \
+            ${interDir}/"${sra}"_paired.collapsed.truncated | \
     samtools view -uh -q 20 -F 0x100 | \
-    samtools sort -o ${interDir}/${sra}.collapsed.sort.bam
-    samtools index ${interDir}/${sra}.collapsed.sort.bam
+    samtools sort -o ${interDir}/"${sra}".collapsed.sort.bam
+    samtools index ${interDir}/"${sra}".collapsed.sort.bam
 
     #merge collapsed and uncollapsed bam files
-      samtools merge ${interDir}/${sra}.mergedbam.bam \
-               ${interDir}/${sra}.collapsed.sort.bam \
-               ${interDir}/${sra}.pairs.sort.bam
-      samtools index ${interDir}/${sra}.mergedbam.bam
+      samtools merge ${interDir}/"${sra}".mergedbam.bam \
+               ${interDir}/"${sra}".collapsed.sort.bam \
+               ${interDir}/"${sra}".pairs.sort.bam
+      samtools index ${interDir}/"${sra}".mergedbam.bam
 
       inputBam=${interDir}/${sra}.mergedbam.bam
   }
@@ -91,14 +91,14 @@ else
   {
     paired="false"
     echo ${paired}
-    ${AdapterRemoval} --file1 ${inputDir}/${sra}_1.fastq --basename ${sra}_unpaired --trimns --trimqualities
+    ${AdapterRemoval} --file1 ${inputDir}/"${sra}"_1.fastq --basename "${sra}"_unpaired --trimns --trimqualities
 
       bwa mem -R "@RG\tID:${sra}\tSM:${identifier}\PL:illumina" \
               /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
-              ${interDir}/${sra}_unpaired.truncated | \
+              ${interDir}/"${sra}"_unpaired.truncated | \
       samtools view -uh -q 20 -F 0x100 | \
-      samtools sort -o ${interDir}/${sra}.unpaired.sort.bam
-      samtools index ${interDir}/${sra}.unpaired.sort.bam
+      samtools sort -o ${interDir}/"${sra}".unpaired.sort.bam
+      samtools index ${interDir}/"${sra}".unpaired.sort.bam
 
       inputBam=${interDir}/${sra}.unpaired.sort.bam
   }
@@ -107,11 +107,11 @@ fi
 echo "Outside of if/else"
 
 #remove duplicates
-java -jar $EBROOTPICARD/picard.jar MarkDuplicates MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=5000 \
-     I=${inputBam} \
-     O=${outputDir}/${sra}.finalmap.bam \
-     METRICS_FILE=${interDir}/${sra}.metrics \
-     COMMENT= ${identifier} \
+java -jar "$EBROOTPICARD"/picard.jar MarkDuplicates MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=5000 \
+     I="${inputBam}" \
+     O=${outputDir}/"${sra}".finalmap.bam \
+     METRICS_FILE=${interDir}/"${sra}".metrics \
+     COMMENT= "${identifier}" \
      REMOVE_DUPLICATES=true \
      CREATE_INDEX=true
 
