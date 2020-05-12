59d3971c4941d66f42624a59dbd70881771cd32b
courtneytern <courtneytern@users.noreply.github.com>
2020-05-12 10:39:07 -0400

    Update mappingscript.sh
    edit slurm settings

--- biosampleresults/mappingscript.sh
+++ biosampleresults/mappingscript.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH -J simCline # A single job name for the array
 #SBATCH --ntasks-per-node=20 # twenty cores
-#SBATCH -N 1
+#SBATCH -N 10
 #SBATCH -t 24:00:00 ### 24 hr
-#SBATCH --mem 10G
+#SBATCH --mem 20G
 #SBATCH -o /scratch/cat7ep/slurmOut/simCline.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/slurmOut/simCline.%A_%a.err # Standard error
 #SBATCH -p standard
