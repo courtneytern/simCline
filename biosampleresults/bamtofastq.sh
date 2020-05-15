ebce1b19cf22fdac607fbc6fcaba0e0664723c7d
courtneytern <courtneytern@users.noreply.github.com>
2020-05-15 11:42:44 -0400

    Update bamtofastq.sh
    syntax, slurm settings

--- biosampleresults/bamtofastq.sh
+++ biosampleresults/bamtofastq.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1
 #SBATCH -t 6:00:00 ### 6 hours
-#SBATCH --mem 1G
+#SBATCH --mem 10G
 #SBATCH -o /scratch/cat7ep/slurmOut/bam2fastq.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/slurmOut/bam2fastq.%A_%a.err # Standard error
 #SBATCH -p standard
@@ -14,11 +14,10 @@
 ##In rivanna, un-comment below
 module load samtools/1.10
 
-##SLURM_ARRAY_TASK_ID=4
 #sbatch /scratch/cat7ep/simCline/biosampleresults/bamtofastq.sh
 
 #each line in destmapped.txt is a file name in /project/berglandlab/dest_mapped/
 /scratch/cat7ep/simCline/destmapped.txt | \
-while read filename; do
-  samtools bam2fq  /project/berglandlab/dest_mapped/$filename/sim.bam > /scratch/cat7ep/fastq/$filename.fastq
+while read -r filename; do
+  samtools bam2fq /project/berglandlab/dest_mapped/"$filename"/sim.bam > /scratch/cat7ep/fastq/"$filename".fastq
 done
