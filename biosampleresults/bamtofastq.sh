d36a45f780dc73f6163cb7e8b5967d5aa9dabc93
courtneytern <courtneytern@users.noreply.github.com>
2020-05-15 11:33:04 -0400

    Update bamtofastq.sh

--- biosampleresults/bamtofastq.sh
+++ biosampleresults/bamtofastq.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-#SBATCH -J simCline # A single job name for the array
+#SBATCH -J bam2fastq # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1
 #SBATCH -t 6:00:00 ### 6 hours
 #SBATCH --mem 1G
-#SBATCH -o /scratch/cat7ep/slurmOut/simCline.%A_%a.out # Standard output
-#SBATCH -e /scratch/cat7ep/slurmOut/simCline.%A_%a.err # Standard error
+#SBATCH -o /scratch/cat7ep/slurmOut/bam2fastq.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/bam2fastq.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
@@ -16,7 +16,9 @@ module load samtools/1.10
 
 ##SLURM_ARRAY_TASK_ID=4
 #sbatch /scratch/cat7ep/simCline/biosampleresults/bamtofastq.sh
-cd /project/berglandlab/dest_mapped/
 
-
-samtools bam2fq  ./*/sim.bam > /scratch/cat7ep/fastq/*.fastq
+#each line in destmapped.txt is a file name in /project/berglandlab/dest_mapped/
+/scratch/cat7ep/simCline/destmapped.txt | \
+while read filename; do
+  samtools bam2fq  ./*/sim.bam > /scratch/cat7ep/fastq/*.fastq
+done
