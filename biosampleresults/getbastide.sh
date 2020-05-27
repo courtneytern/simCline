b20a18928e4637571c89a02c12c09c0b8cb1d816
courtneytern <courtneytern@users.noreply.github.com>
2020-05-27 12:03:43 -0400

    Update getbastide.sh
    add dependencies

--- biosampleresults/getbastide.sh
+++ biosampleresults/getbastide.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+module load gcc/9.2.0
+module load mvapich2/2.3.3
+module load gcc/9.2.0
+module load openmpi/3.1.4
+module load python/3.7.7
 
 #groupget command pulls all files from one project number from ENA
 alias enaGroupGet="/scratch/cat7ep/simCline/biosampleresults/enaBrowserTools/python3/enaGroupGet"
