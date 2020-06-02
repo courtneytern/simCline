284aa5949a5801f4eaae82af2aa51850a19a995c
courtneytern <courtneytern@users.noreply.github.com>
2020-06-02 12:02:41 -0400

    Update checkfastq.sh

--- biosampleresults/checkfastq.sh
+++ biosampleresults/checkfastq.sh
@@ -4,11 +4,6 @@ id=$( cat /scratch/cat7ep/simCline2err.txt | awk -F" " '{
    print array[2]
 }' )
 
-#only the ones that are not ok
-notOK=$( cat /scratch/cat7ep/simCline/biosampleresults/notok.txt | awk -F" " '{
-  print $1
-}' )
-
 for n in $id; do
 SRS=$( grep ^$n"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
   awk -F"," '{
@@ -23,4 +18,20 @@ SRS=$( grep ^$n"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | 
   else
     echo "$n NOT OK"
   fi
+done
+
+#only the ones that are not ok
+notOK=$( cat /scratch/cat7ep/simCline/biosampleresults/notok.txt | awk -F" " '{
+  print $1
+}' )
+
+for n in $notOK; do
+SRS=$( grep ^$n"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[16]
+    print SRSnum
+  }' )
+
+  fasterq-dump ${SRS} -t /scratch/cat7ep/interDir --outdir /scratch/cat7ep/fasterq/
 done
