591dd0c336ac744f762c963379476829dda72977
courtneytern <courtneytern@users.noreply.github.com>
2020-06-02 12:01:33 -0400

    Checking fastq

---
+++ biosampleresults/checkfastq.sh
@@ -0,0 +1,26 @@
+#initial check
+id=$( cat /scratch/cat7ep/simCline2err.txt | awk -F" " '{
+   split($1,array,"_")
+   print array[2]
+}' )
+
+#only the ones that are not ok
+notOK=$( cat /scratch/cat7ep/simCline/biosampleresults/notok.txt | awk -F" " '{
+  print $1
+}' )
+
+for n in $id; do
+SRS=$( grep ^$n"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[16]
+    print SRSnum
+  }' )
+
+  if [ -f /scratch/cat7ep/fasterq/${SRS}.fastq ] || [ -f /scratch/cat7ep/fasterq/${SRS}_1.fastq ]
+  then
+    echo "$n ok"
+  else
+    echo "$n NOT OK"
+  fi
+done
