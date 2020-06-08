680f8f8effbe2d3c8ae9c343e7182127709e5a55
courtneytern <courtneytern@users.noreply.github.com>
2020-06-08 13:17:15 -0400

    Update mappingscript.sh

--- biosampleresults/mappingscript.sh
+++ biosampleresults/mappingscript.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH -J mapping # A single job name for the array
 #SBATCH --ntasks-per-node=20 # twenty cores
-#SBATCH -N 2
+#SBATCH -N 1
 #SBATCH -t 24:00:00 ### 24 hr
-#SBATCH --mem 30G
+#SBATCH --mem 0
 #SBATCH -o /scratch/cat7ep/slurmOut/mapping.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/slurmOut/mapping.%A_%a.err # Standard error
 #SBATCH -p standard
