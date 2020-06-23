38c31c5b851cd620fb2775ff75cee738686700d0
courtneytern <courtneytern@users.noreply.github.com>
2020-06-23 12:55:39 -0400

    pooled vcf

--- biosampleresults/vcf2gds.R
+++ biosampleresults/vcf2gds.R
@@ -2,6 +2,21 @@
 
 library(SeqArray)
 
-vcf.file <- "/project/berglandlab/courtney/simCline/pooledData.vcf"
-gds.output <- "/project/berglandlab/courtney/simCline/pooledData.gds"
+vcf.file <- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.vcf"
+gds.output <- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.gds"
 seqVCF2GDS(vcf.fn = vcf.file, out.fn = gds.output)
+
+gds.file <- seqOpen(gds.output)
+snp.dt <- data.frame(chr=seqGetData(gds.file, "chromosome"),
+                     pos=seqGetData(gds.file, "position"),
+                     nAlleles=seqGetData(gds.file, "$num_allele"),
+                     id=seqGetData(gds.file, "variant.id"),
+                     seqMissing(gds.file, per.variant=T))
+table(snp.dt$chr)
+prop.table(table(snp.dt$nAlleles))
+
+#now filter
+nAlleles=seqGetData(gds.file, "$num_allele")
+seqSetFilter(gds.file, variant.id=snp.dt[which(snp.dt$nAlleles==2), ]$id)
+adList<- seqGetData(gds.file, "annotation/format/AD")
+rdList<- seqGetData(gds.file, "annotation/format/RD" )
