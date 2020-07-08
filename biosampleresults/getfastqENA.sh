ebc6084a39b64c0296b17f550315df68197bc7c3
courtneytern <courtneytern@users.noreply.github.com>
2020-07-08 18:15:15 -0400

    Housekeeping
    removing unnecessary files and renaming files to be more clear

---
+++ biosampleresults/getfastqENA.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+module load gcc/9.2.0
+module load mvapich2/2.3.3
+module load gcc/9.2.0
+module load openmpi/3.1.4
+module load python/3.7.7
+
+#groupget command pulls all files from one project number from ENA
+alias enaGroupGet="/scratch/cat7ep/simCline/biosampleresults/enaBrowserTools/python3/enaGroupGet"
+
+enaGroupGet -f fastq -d /scratch/cat7ep/africafastq PRJEB7673
