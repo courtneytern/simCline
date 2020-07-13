c4bc8b23a92e44a9e380a9e0bb8eb82679133fed
courtneytern <courtneytern@users.noreply.github.com>
2020-07-13 10:30:17 -0400

    Update gunzipJackson.sh

--- biosampleresults/gunzipJackson.sh
+++ biosampleresults/gunzipJackson.sh
@@ -3,8 +3,8 @@
 while read line
 do
 {
-   cd /scratch/cat7ep/fastqJackson/PRJEB7673/{$line}
-   gunzip ./{$line}_1 > /scratch/cat7ep/fastqJackson/{$line}_1.fastq
-   gunzip ./{$line}_2 > /scratch/cat7ep/fastqJackson/{$line}_2.fastq
+   cd /scratch/cat7ep/fastqJackson/PRJEB7673/$line
+   gunzip ./$line_1 > /scratch/cat7ep/fastqJackson/$line_1.fastq
+   gunzip ./$line_2 > /scratch/cat7ep/fastqJackson/$line_2.fastq
 }
 done <  /scratch/cat7ep/simCline/biosampleresults/jacksonAccessionNos.txt
