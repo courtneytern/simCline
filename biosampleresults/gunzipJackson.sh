08ab67e8480f9c166c374bd79a6e7f8ac6bef87c
courtneytern <courtneytern@users.noreply.github.com>
2020-07-13 10:23:45 -0400

    Update gunzipJackson.sh

--- biosampleresults/gunzipJackson.sh
+++ biosampleresults/gunzipJackson.sh
@@ -1,8 +1,10 @@
 #unzip all files from the Jackson fastq folder and move them to one location
 
 for line in /scratch/cat7ep/simCline/biosampleresults/jacksonAccessionNos.txt
+do
 {
    cd /scratch/cat7ep/fastqJackson/PRJEB7673/{$line}
    gunzip ./{$line}_1 > /scratch/cat7ep/fastqJackson/{$line}_1.fastq
    gunzip ./{$line}_2 > /scratch/cat7ep/fastqJackson/{$line}_2.fastq
 }
+done
