6689980fd6e6f4276b2df0842d8323a087ca64b5
courtneytern <courtneytern@users.noreply.github.com>
2020-07-13 10:21:54 -0400

    unzipping Jackson fastq files

---
+++ biosampleresults/gunzipJackson.sh
@@ -0,0 +1,8 @@
+#unzip all files from the Jackson fastq folder and move them to one location
+
+for line in /scratch/cat7ep/simCline/biosampleresults/jacksonAccessionNos.txt
+{
+   cd /scratch/cat7ep/fastqJackson/PRJEB7673/{$line}
+   gunzip ./{$line}_1 > /scratch/cat7ep/fastqJackson/{$line}_1.fastq
+   gunzip ./{$line}_2 > /scratch/cat7ep/fastqJackson/{$line}_2.fastq
+}
