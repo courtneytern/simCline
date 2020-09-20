c363eefbb573846ca5c22915b0fa1806e14fedbe
courtneytern <courtneytern@users.noreply.github.com>
2020-09-20 16:20:00 -0400

    Update pooledFilter.sh

--- biosampleresults/pooledFilter.sh
+++ biosampleresults/pooledFilter.sh
@@ -7,26 +7,28 @@ bamDir="/project/berglandlab/courtney/simCline/bamfinal"
 cd /scratch/cat7ep/simCline/biosampleresults/
 
 ###make list of input bam files
-sra=$( grep ",P," ./concatenated.csv| \
+#first take the pooled samples that don't include european
+sra=$( grep ",P," ./concatenated.csv | grep -v "DrosEU" | \
   awk -F"," '{
     split ($0,array,",")
     SRSnum= array[16]
     print SRSnum
   }' )
-# idEuro=$( grep ",P," ./concatenatedEuro.csv| \
-#   awk -F"," '{
-#     split ($0,array,",")
-#     id= array[17]
-#     print id
-#   }' )
+#now get european
+idEuro=$( grep ",P," ./concatenatedEuro.csv| \
+  awk -F"," '{
+    split ($0,array,",")
+    id= array[16]
+    print id
+  }' )
 
 for line in $sra; do
   echo $line >> /scratch/cat7ep/simCline/biosampleresults/inputPooledSamps.txt
   echo $bamDir/$line.finalmap.bam >> /scratch/cat7ep/simCline/biosampleresults/inputPooledBam.txt
 done
-# for line in $idEuro; do
-#   echo $line >> /scratch/cat7ep/simCline/biosampleresults/inputPooledSamps.txt
-#   echo $bamDir/$line.finalmap.bam >> /scratch/cat7ep/simCline/biosampleresults/inputPooledBam.txt
-# done
+for line in $idEuro; do
+  echo $line >> /scratch/cat7ep/simCline/biosampleresults/inputPooledSamps.txt
+  echo $bamDir/$line.finalmap.bam >> /scratch/cat7ep/simCline/biosampleresults/inputPooledBam.txt
+done
 
 #go to makeVCF.sh
