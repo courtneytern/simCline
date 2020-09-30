f899269b0b922be33460fc42e609cb2d59c078b9
courtneytern <courtneytern@users.noreply.github.com>
2020-09-30 12:23:27 -0400

    Update verifybam.sh

--- biosampleresults/verifybam.sh
+++ biosampleresults/verifybam.sh
@@ -1,18 +1,27 @@
-SRS=$( grep ^[1-826]"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
-  awk -F"," '{
-    split ($0,array,",")
-    SRSnum= array[15]
-    print SRSnum
-  }' )
+# SRS=$( grep ^[1-826]"," /scratch/cat7ep/simCline/biosampleresults/concatenated.csv | \
+#   awk -F"," '{
+#     split ($0,array,",")
+#     SRSnum= array[15]
+#     print SRSnum
+#   }' )
+#
+# if [ -f /project/berglandlab/courtney/simCline/bamfiles/*${SRS}* ]
+# then
+# {
+#    echo ""
+# }
+#
+# else
+# {
+#    echo ${SRS}
+# }
+# fi
 
-if [ -f /project/berglandlab/courtney/simCline/bamfiles/*${SRS}* ]
-then
-{
-   echo ""
+#verify from inputPooledBam.txt
+for line in /scratch/cat7ep/simCline/biosampleresults/inputPooledBam.txt {
+  if [ -f "${line}" ]; then
+    echo ""
+  else
+    echo "${line}"
+  fi
 }
-
-else
-{
-   echo ${SRS}
-}
-fi
