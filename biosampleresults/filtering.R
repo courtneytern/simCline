6b243f2994599b71e55cbe5d675a14feb197d34b
courtneytern <courtneytern@users.noreply.github.com>
2020-10-12 15:42:39 -0400

    reshuffling vcf/filtering/lea files

--- biosampleresults/filtering.R
+++ biosampleresults/filtering.R
@@ -1,6 +1,5 @@
-# lea analysis
+# this generates the freqAlt plots per pooled population
 
-library(LEA)
 library(SeqArray)
 library(ggplot2)
 library(data.table)
@@ -48,25 +47,3 @@ dat.merged <- merge(dat, concatenated)
 
 #plot frequency of alternate alleles for each of the 422 populations
 ggplot(data=dat, aes(x=freqAlt)) + geom_histogram() + facet_wrap(~identifier)
-
-#################LEA###############
-setwd("~/Downloads/GitHub/simCline/biosampleresults/")
-
-vcf.file<- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.vcf"
-lcmm.file<- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.lfmm"
-vcf2lfmm(vcf.file, lcmm.file, force = TRUE)
-pc<- pca("~/Downloads/GitHub/simCline/biosampleresults/pooledData.lfmm")
-
-# Display information on analysis.
-show(pc)
-# Summarize analysis.
-summary(pc)
-par(mfrow=c(2,2))
-# Plot eigenvalues.
-plot(pc, lwd=5, col="blue", cex = .7, xlab=("Factors"), ylab="Eigenvalues")
-# PC1-PC2 plot.
-plot(pc$projections)
-# PC3-PC4 plot.
-plot(pc$projections[,3:4])
-# Plot standard deviations.
-plot(pc$sdev)
--- biosampleresults/vcf2gds.R
+++ biosampleresults/vcf2gds.R
@@ -22,34 +22,4 @@ snp.dt <- data.table(chr=seqGetData(gds.file, "chromosome"),
 # #filtering where chromosome is not Dmel 
 snp.dt <- snp.dt[grepl("Dsim_Scf_2L|Dsim_Scf_2R|Dsim_Scf_3L|Dsim_Scf_3R", chr)]
 
-#########old code. go to filtering.R
-# table(snp.dt$chr)
-# prop.table(table(snp.dt$nAlleles))
-# ##keep only where 2 alleles
-# nAlleles=seqGetData(gds.file, "$num_allele")
-# seqSetFilter(gds.file, variant.id=snp.dt[nAlleles==2]$id[1:1000])
-# 
-# adList<- seqGetData(gds.file, "annotation/format/AD")
-# rdList<- seqGetData(gds.file, "annotation/format/RD")
-# 
-# 
-# dat <- data.table(ad=expand.grid(adList$data)$Var1,
-#                   rd=expand.grid(rdList$data)$Var1,
-#                   population=rep(seqGetData(gds.file, "sample.id"), dim(adList$data)[2]),
-#                   variant.id=rep(seqGetData(gds.file, "variant.id"), each=dim(adList$data)[1]))
-# dat[,freqAlt:=ad/(ad+rd)]
-# 
-# dat.ag <- dat[,list(population=population,nmissing=sum(is.na(ad)), aveAD=mean(ad, na.rm=T), freqAlt=sum(ad, na.rm=T)/sum(ad+rd, na.rm=T)), list(variant.id)]
-# 
-# #calculate the unweighted frequency of alt alleles
-# unweightFreqAlt<-mean(dat.ag$freqAlt)
-# unweightFreqAlt
-# 
-# ggplot(data=dat, aes(x=freqAlt)) + geom_histogram() + facet_wrap(~population)
-# 
-# #calculate missing rate
-# freqMissing<-dat.ag$nmissing/sum(dat$ad,dat$rd,dat.ag$nmissing,na.rm=T)
-# mean(freqMissing)
-# var(freqMissing)
-# 
-# ggplot(data=dat.ag, aes(x=freqMissing)) + geom_histogram()
+#go to filtering.R
