c205c9227b3e2dd066401c42cac9052cb099eb82
courtneytern <courtneytern@users.noreply.github.com>
2020-10-27 13:36:24 -0400

    Update lea.sh

--- biosampleresults/lea.sh
+++ biosampleresults/lea.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1
 #SBATCH -t 6:00:00 ### 6 hours
-#SBATCH --mem 30G
+#SBATCH --mem 0
 #SBATCH -o /scratch/cat7ep/slurmOut/lea.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/slurmOut/lea.%A_%a.err # Standard error
 #SBATCH -p standard
