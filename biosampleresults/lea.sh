9e9bb4352534b6dba172efb950639161d2f5dbe9
courtneytern <courtneytern@users.noreply.github.com>
2020-10-27 13:44:11 -0400

    slurm settings

--- biosampleresults/lea.sh
+++ biosampleresults/lea.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH -J lea # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
-#SBATCH -N 3
+#SBATCH -N 1
 #SBATCH -t 6:00:00 ### 6 hours
-#SBATCH --mem 0
+#SBATCH --mem 300
 #SBATCH -o /scratch/cat7ep/slurmOut/lea.%A_%a.out # Standard output
 #SBATCH -e /scratch/cat7ep/slurmOut/lea.%A_%a.err # Standard error
 #SBATCH -p standard
