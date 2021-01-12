c04fc4726455a0f8ac6c8e111963bb514f27253b
courtneytern <courtneytern@users.noreply.github.com>
2021-01-12 12:43:31 -0500

    Update 1.run_fastqc_reads.sh
    add in better slurm settings

--- biosampleresults/1.run_fastqc_reads.sh
+++ biosampleresults/1.run_fastqc_reads.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-
+#
+#SBATCH -J fastqc # A single job name for the array
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem=20G
 #SBATCH --time=72:00:00
 #SBATCH --partition=standard
+#SBATCH -o /scratch/cat7ep/slurmOut/fastqc.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/fastqc.%A_%a.err # Standard error
 #SBATCH --account=berglandlab
 
 
+######sbatch /scratch/cat7ep/simCline/biosampleresults/1.run_fastqc_reads.sh 
 #Load fastqc
 module load fastqc
 
