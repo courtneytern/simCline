8c79d58808d6d9667174fbf699a7b6f22ca325c0
Courtney Tern <catern19@gmail.com>
2021-02-24 11:53:57 -0500

    Create deleteUnpairedMachado.sh

---
+++ biosampleresults/deleteUnpairedMachado.sh
@@ -0,0 +1,20 @@
+## delete unpaired Machado fastq files
+cd /scratch/cat7ep/simCline/biosampleresults
+fastqDir=/scratch/cat7ep/fasterq
+
+#get the SRS numbers from the Machado files only
+machado=$( grep ",Machado," ./concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[16]
+    print SRSnum
+  }' )
+
+for line in $machado; do
+  if [[ -e "${fastqDir}/${line}.fastq" ]] # if the unpaired file exists
+  then
+    rm ${fastqDir}/${line}.fastq # remove it
+  fi # otherwise do nothing
+done
+
+##QC: SRR2397083 doesn't exist as unpaired. SRR2859127 does exist as unpaired
