f375407d8a721d9b4b5b3e6749120294df4995d7
Courtney Tern <catern19@gmail.com>
2021-02-25 10:56:39 -0500

    Rename script

--- biosampleresults/cleaning_editingScripts/deleteUnpairedFastq.sh
+++ biosampleresults/cleaning_editingScripts/deleteUnpairedFastq.sh
@@ -8,7 +8,7 @@ machado=$( grep "Machado" ./concatenated.csv | \
     split ($0,array,",")
     SRSnum= array[16]
     print SRSnum
-  }' ) 
+  }' )
 
 for line in $machado; do
   if [[ -e "${fastqDir}/${line}.fastq" ]] # if the unpaired file exists
@@ -18,3 +18,22 @@ for line in $machado; do
 done
 
 ##QC: SRR2397083 doesn't exist as unpaired. SRR2859127 does exist as unpaired
+
+## delete unpaired Signor fastq files
+cd /scratch/cat7ep/simCline/biosampleresults
+fastqDir=/scratch/cat7ep/fasterq
+
+#get the SRS numbers from the Machado files only
+signor=$( grep "Signor" ./concatenated.csv | \
+  awk -F"," '{
+    split ($0,array,",")
+    SRSnum= array[16]
+    print SRSnum
+  }' )
+
+for line in $signor; do
+  if [[ -e "${fastqDir}/${line}.fastq" ]] # if the unpaired file exists
+  then
+    rm ${fastqDir}/${line}.fastq # remove it
+  fi # otherwise do nothing
+done
