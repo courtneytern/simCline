96594ac52a45e2c1502046d3f59dbda61d41d17b
Courtney Tern <catern19@gmail.com>
2021-06-07 10:43:08 -0400

    Update 2.Trim_and_Map.sh

--- biosampleresults/2.Trim_and_Map.sh
+++ biosampleresults/2.Trim_and_Map.sh
@@ -6,8 +6,8 @@
 #SBATCH --time=8:00:00
 #SBATCH --partition=standard
 #SBATCH --account=berglandlab
-#SBATCH -o /scratch/cat7ep/slurmOut/trimmap3.%A_%a.out # Standard output
-#SBATCH -e /scratch/cat7ep/slurmOut/trimmap3.%A_%a.err # Standard error
+#SBATCH -o /scratch/cat7ep/slurmOut/trimmap4.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/trimmap4.%A_%a.err # Standard error
 #SBATCH --array=1-576
 
 ####### sbatch /scratch/cat7ep/simCline/biosampleresults/2.Trim_and_Map.sh
