7b4fd0b7880032ac828d51ed974354fb0de2627f
Courtney Tern <catern19@gmail.com>
2021-07-25 12:12:30 -0400

    Update vcf2gds.R

--- biosampleresults/vcf2gds.R
+++ biosampleresults/vcf2gds.R
@@ -23,8 +23,8 @@ seqVCF2GDS(vcf.fn = vcf.file, out.fn = gds.output)
 #########################
 
 #load vcf and gds files 
-vcf.file <- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.vcf"
-gds.output <- "~/Downloads/GitHub/simCline/biosampleresults/pooled.gds"
+vcf.file <- "~/Downloads/GitHub/simCline/biosampleresults/Simcline_final_2021.vcf.gz"
+gds.output <- "~/Downloads/GitHub/simCline/biosampleresults/individ.gds"
 #create GDS file 
 seqVCF2GDS(vcf.fn = vcf.file, out.fn = gds.output)
 
