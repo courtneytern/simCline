b7a508f84e6a7261762defc5b5e1ac32f8f97c59
Courtney Tern <catern19@gmail.com>
2021-10-06 13:03:47 -0400

    slurm vcf2gds.R

---
+++ biosampleresults/vcf2gds.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=120G
+#SBATCH --time=24:00:00
+#SBATCH --partition=largemem
+#SBATCH --account=berglandlab_standard
+#SBATCH -o /scratch/cat7ep/slurmOut/vcf2gds100621.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/vcf2gds100621.%A_%a.err # Standard error
+
+module load gcc/7.1.0  openmpi/3.1.4  intel/18.0  intelmpi/18.0
+module load R
+
+/scratch/cat7ep/simCline/biosampleresults/vcf2gds.R
