e75d54adc6b300449a5e70c01f897d69f5584b40
Courtney Tern <catern19@gmail.com>
2021-10-06 13:03:35 -0400

    comment out pooled

--- biosampleresults/vcf2gds.R
+++ biosampleresults/vcf2gds.R
@@ -8,10 +8,10 @@ library(data.table)
 #########################
 
 #load vcf and gds files 
-vcf.file <- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.vcf"
-gds.output <- "~/Downloads/GitHub/simCline/biosampleresults/pooled.gds"
-#create GDS file 
-seqVCF2GDS(vcf.fn = vcf.file, out.fn = gds.output)
+# vcf.file <- "~/Downloads/GitHub/simCline/biosampleresults/pooledData.vcf"
+# gds.output <- "~/Downloads/GitHub/simCline/biosampleresults/pooled.gds"
+# #create GDS file 
+# seqVCF2GDS(vcf.fn = vcf.file, out.fn = gds.output)
 
 #########################################
 
