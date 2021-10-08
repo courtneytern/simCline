cde4df9ca6d9945cc1fef0ddd4f4f2b85b78c149
Courtney Tern <catern19@gmail.com>
2021-10-07 22:17:42 -0400

    Update vcf2gds.sh

--- biosampleresults/vcf2gds.sh
+++ biosampleresults/vcf2gds.sh
@@ -3,11 +3,11 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=2
 #SBATCH --mem=120G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=largemem
 #SBATCH --account=berglandlab_standard
-#SBATCH -o /scratch/cat7ep/slurmOut/vcf2gds100621.%A_%a.out # Standard output
-#SBATCH -e /scratch/cat7ep/slurmOut/vcf2gds100621.%A_%a.err # Standard error
+#SBATCH -o /scratch/cat7ep/slurmOut/vcf2gds100721.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/vcf2gds100721.%A_%a.err # Standard error
 
 module load gcc/7.1.0  openmpi/3.1.4  intel/18.0  intelmpi/18.0
 module load R
