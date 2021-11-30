7b3ff1d5a49c06339c6160f79481e2058ea4b51a
Courtney Tern <catern19@gmail.com>
2021-11-29 22:36:55 -0500

    increase mem alloc

--- biosampleresults/combinePooledIndivid.sh
+++ biosampleresults/combinePooledIndivid.sh
@@ -3,6 +3,7 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=20G
+#SBATCH --mem-per-cpu=20G
 #SBATCH --time=24:00:00
 #SBATCH --partition=standard
 #SBATCH --account=berglandlab_standard
