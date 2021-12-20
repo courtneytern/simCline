c107fee897b3e3c0787916df2ea7caef5d12a28a
Courtney Tern <catern19@gmail.com>
2021-12-20 10:13:48 -0500

    Create combineTreemixInputs.R

---
+++ biosampleresults/combineTreemixInputs.R
@@ -0,0 +1,21 @@
+# merge pooled and individ treemix input files
+## To be run on rivanna
+
+# module load gcc/7.1.0  openmpi/3.1.4  R
+# R 
+
+library(data.table)
+
+setwd("/scratch/cat7ep/simCline/biosampleresults")
+individInput<- fread("./treemixIndividInput_FINAL.txt",header = TRUE ,sep=" ")
+pooledInput<- fread("./treemixPooledInput_FINAL.txt",header = TRUE ,sep=" ")
+
+setkeyv(individInput,c("chr","pos"))
+setkeyv(pooledInput,c("chrom","pos"))
+combined<- merge(individInput,pooledInput,by.x=c("chr","pos"),by.y=c("chrom","pos"))
+
+# drop the chr and pos cols now 
+finalInput<- combined[,c(-1,-2)]
+fwrite(finalInput,file="./treemixCOMBINEDinput.txt",quote=F,sep = " ")
+
+# ready to run treemixs.sh now
