b6909acb05be471453e8a54c4db28c535669cbdb
Courtney Tern <catern19@gmail.com>
2021-12-22 23:36:54 -0500

    update time limit

--- biosampleresults/f3.sh
+++ biosampleresults/f3.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=20G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=standard
 #SBATCH --account=berglandlab_standard
 #SBATCH -o /scratch/cat7ep/slurmOut/f3.%A_%a.out # Standard output
