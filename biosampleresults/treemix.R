d75a8c59606d696258fd716fd0572a5756c04ef7
Courtney Tern <catern19@gmail.com>
2022-01-13 16:21:49 -0500

    draw final treemix figure
    combine all four migration plots into one figure, add appropriate labels

--- biosampleresults/treemix.R
+++ biosampleresults/treemix.R
@@ -6,10 +6,19 @@
 
 
 setwd("/scratch/cat7ep/simCline/biosampleresults/treemixOutputs")
-# setwd("~/Downloads/GitHub/simCline/biosampleresults")
+# setwd("~/Downloads/GitHub/simCline/biosampleresults/treemixOutputs")
 
 source("/scratch/cat7ep/simCline/biosampleresults/treemix-1.13/src/plotting_funcs.R")
+# source("~/Downloads/GitHub/simCline/biosampleresults/treemix-1.13/src/plotting_funcs.R")
 # run the following for m0,1,2,3
-pdf("./m3.pdf")
+pdf("./m0.pdf")
+par(adj=0,cex=0.5,mar=c(2,4,4,4),mfrow=c(2,2))
+plot_tree("simcline_treeOut_m0") 
+  mtext("(A)",side=3,adj=0,cex=3)
+plot_tree("simcline_treeOut_m1")
+  mtext("(B)",side=3,adj=0,cex=3)
+plot_tree("simcline_treeOut_m2")
+  mtext("(C)",side=3,adj=0,cex=3)
 plot_tree("simcline_treeOut_m3")
+  mtext("(D)",side=3,adj=0,cex=3)
 dev.off()
