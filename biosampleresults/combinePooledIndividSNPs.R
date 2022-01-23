d8a4ba125ebd70350946da26d44c40bdd189b90e
Courtney Tern <catern19@gmail.com>
2022-01-23 11:28:12 -0500

    rerunning individ
    add in filtering for biallelic only

--- biosampleresults/combinePooledIndividSNPs.R
+++ biosampleresults/combinePooledIndividSNPs.R
@@ -1,6 +1,6 @@
 # Get only the SNPs that overlap in both pooled and individual populations
 # Make a venn diagram showing this overlap 
-## To be run on Rivanna
+## To be run on Rivanna via **** combinePooledIndivid.sh ****
 
 # module load gcc/7.1.0  openmpi/3.1.4  R
 # R 
@@ -44,83 +44,83 @@ joined_snps<- intersect(individ.dt,pooled.dt)
 ## rerun the treemix formatting for each 
 
 ## Pooled  #### 
-pooledPath<- "/scratch/cat7ep/simCline/biosampleresults/pooled.gds"
-makePooledTreemix<- function(pooledPath)  {
-  gds.file<- seqOpen(pooledPath,allow.duplicate = T)
-  # keep only joined snps
-  seqSetFilterPos(gds.file,chr=joined_snps$chr,pos=joined_snps$pos)
-
-  snp.dt <- data.table(chr=seqGetData(gds.file, "chromosome"),
-                       pos=seqGetData(gds.file, "position"),
-                       nAlleles=seqGetData(gds.file, "$num_allele"),
-                       id=seqGetData(gds.file, "variant.id"),
-                       seqMissing(gds.file, per.variant=T))
-  # filter where not dmels
-  snp.dt <- snp.dt[grepl("Dsim_Scf_2L|Dsim_Scf_2R|Dsim_Scf_3L|Dsim_Scf_3R", chr)]
-  # keep only where 2 alleles
-  nAlleles=seqGetData(gds.file, "$num_allele")
-  # get ids for previous filters
-  ids <- snp.dt[nAlleles==2]$id
-  seqSetFilter(gds.file, variant.id=ids)
-
-  ## Get AD/RD list
-  adList<- seqGetData(gds.file, "annotation/format/AD")
-  rdList<- seqGetData(gds.file, "annotation/format/RD")
-  # compile variables of interest
-  dat <- data.table(population=rep(seqGetData(gds.file, "sample.id"), dim(adList$data)[2]),
-                   variant.id=rep(seqGetData(gds.file, "variant.id"), each=dim(adList$data)[1]),
-                    ad=expand.grid(adList$data)$Var1,
-                    rd=expand.grid(rdList$data)$Var1,
-                    position=rep(seqGetData(gds.file, "position"), each=dim(adList$data)[1]),
-                    chromosome=rep(seqGetData(gds.file, "chromosome"), each=dim(adList$data)[1])
-  )
-  dat.ag2 <- dat[,list(nmissing=mean(is.na(ad)), aveAD=mean(ad, na.rm=T), aveRD=mean(rd, na.rm=T),
-                      freqAlt=sum(ad, na.rm=T)/sum(ad+rd, na.rm=T),
-                       chrom= chromosome[1], pos= position[1]),
-                 list(variant.id)]
-  # aggregate duplicate Machado rows
-  agg<- dat %>% group_by(population,variant.id,position,chromosome) %>% summarise_all(sum)
-  agg<- as.data.table(agg)
-  # now calc freq alt
-  agg[,freqAlt:=ad/(ad+rd)]
-
-  # calc ad/rd per variant id -- merge dat.ag2 aveAD and aveRD with dat
-  setkey(agg,variant.id)
-  setkey(dat.ag2,variant.id)
-  # left outer join. necessitate keeping all of dat
-  ## subset dat.ag2 to be just the variant id, aveAD, aveRD, chrom, pos
-  merged <- merge(agg, dat.ag2[,c(1,3,4,6,7)], all.x=TRUE)
-
-  # if NA in either ad or rd, construct treemix "x,y" with both avgs
-  ## if not NA, use the actual val for "x,y"
-  # take as parameters: ad col, rd col, aveAD col, aveRD col
-  checkNA <- function(a,r,aa,ar){
-  if( is.na(a)|is.na(r) ) {
-    return(paste(aa,ar,sep=","))
-  } # if
-  else
-    return(paste(a,r,sep=","))
-  }
-  merged[,newCol:=mapply(checkNA,merged$ad,merged$rd,merged$aveAD,merged$aveRD)]
-  # long to wide
-  datw <- dcast(merged, chrom+pos ~ population, value.var="newCol")
-  # drop the var.id column
-  #datw <- datw[,-1]
-  # rename columns to something shorter
-  colnames(datw)
-  newColNames <- c("chrom","pos",
-                   "Barghi:FL:Tallahassee","ES_Gim_14_34","ES_Gim_14_35","ES_Gim_16_33",
-                   "ES_Pur_16_35","FR_Got_15_48","IT_Mez_15_43","IT_Mez_15_44","IT_Tre_16_15",
-                   "Machado:Linvilla:65","Machado:Linvilla:50","PT_Rec_15_16","Sedghifar:SC:Conway",
-                   "Sedghifar:ME:Fairfield","Sedghifar:FL:Miami","Sedghifar:Panama",
-                   "Sedghifar:NJ:Princeton","Sedghifar:RI:Providence","Sedghifar:NC:Raleigh",
-                   "Sedghifar:VA:Richmond","Sedghifar:GA:Savannah" )
-
-  colnames(datw) <- newColNames
-  datw
-}
-pooledInput<- makePooledTreemix(pooledPath)
-write.table(pooledInput,"/scratch/cat7ep/simCline/biosampleresults/treemixPooledInput.txt",row.names = F)
+# pooledPath<- "/scratch/cat7ep/simCline/biosampleresults/pooled.gds"
+# makePooledTreemix<- function(pooledPath)  {
+#   gds.file<- seqOpen(pooledPath,allow.duplicate = T)
+#   # keep only joined snps
+#   seqSetFilterPos(gds.file,chr=joined_snps$chr,pos=joined_snps$pos)
+# 
+#   snp.dt <- data.table(chr=seqGetData(gds.file, "chromosome"),
+#                        pos=seqGetData(gds.file, "position"),
+#                        nAlleles=seqGetData(gds.file, "$num_allele"),
+#                        id=seqGetData(gds.file, "variant.id"),
+#                        seqMissing(gds.file, per.variant=T))
+#   # filter where not dmels
+#   snp.dt <- snp.dt[grepl("Dsim_Scf_2L|Dsim_Scf_2R|Dsim_Scf_3L|Dsim_Scf_3R", chr)]
+#   # keep only where 2 alleles
+#   nAlleles=seqGetData(gds.file, "$num_allele")
+#   # get ids for previous filters
+#   ids <- snp.dt[nAlleles==2]$id
+#   seqSetFilter(gds.file, variant.id=ids)
+# 
+#   ## Get AD/RD list
+#   adList<- seqGetData(gds.file, "annotation/format/AD")
+#   rdList<- seqGetData(gds.file, "annotation/format/RD")
+#   # compile variables of interest
+#   dat <- data.table(population=rep(seqGetData(gds.file, "sample.id"), dim(adList$data)[2]),
+#                    variant.id=rep(seqGetData(gds.file, "variant.id"), each=dim(adList$data)[1]),
+#                     ad=expand.grid(adList$data)$Var1,
+#                     rd=expand.grid(rdList$data)$Var1,
+#                     position=rep(seqGetData(gds.file, "position"), each=dim(adList$data)[1]),
+#                     chromosome=rep(seqGetData(gds.file, "chromosome"), each=dim(adList$data)[1])
+#   )
+#   dat.ag2 <- dat[,list(nmissing=mean(is.na(ad)), aveAD=mean(ad, na.rm=T), aveRD=mean(rd, na.rm=T),
+#                       freqAlt=sum(ad, na.rm=T)/sum(ad+rd, na.rm=T),
+#                        chrom= chromosome[1], pos= position[1]),
+#                  list(variant.id)]
+#   # aggregate duplicate Machado rows
+#   agg<- dat %>% group_by(population,variant.id,position,chromosome) %>% summarise_all(sum)
+#   agg<- as.data.table(agg)
+#   # now calc freq alt
+#   agg[,freqAlt:=ad/(ad+rd)]
+# 
+#   # calc ad/rd per variant id -- merge dat.ag2 aveAD and aveRD with dat
+#   setkey(agg,variant.id)
+#   setkey(dat.ag2,variant.id)
+#   # left outer join. necessitate keeping all of dat
+#   ## subset dat.ag2 to be just the variant id, aveAD, aveRD, chrom, pos
+#   merged <- merge(agg, dat.ag2[,c(1,3,4,6,7)], all.x=TRUE)
+# 
+#   # if NA in either ad or rd, construct treemix "x,y" with both avgs
+#   ## if not NA, use the actual val for "x,y"
+#   # take as parameters: ad col, rd col, aveAD col, aveRD col
+#   checkNA <- function(a,r,aa,ar){
+#   if( is.na(a)|is.na(r) ) {
+#     return(paste(aa,ar,sep=","))
+#   } # if
+#   else
+#     return(paste(a,r,sep=","))
+#   }
+#   merged[,newCol:=mapply(checkNA,merged$ad,merged$rd,merged$aveAD,merged$aveRD)]
+#   # long to wide
+#   datw <- dcast(merged, chrom+pos ~ population, value.var="newCol")
+#   # drop the var.id column
+#   #datw <- datw[,-1]
+#   # rename columns to something shorter
+#   colnames(datw)
+#   newColNames <- c("chrom","pos",
+#                    "Barghi:FL:Tallahassee","ES_Gim_14_34","ES_Gim_14_35","ES_Gim_16_33",
+#                    "ES_Pur_16_35","FR_Got_15_48","IT_Mez_15_43","IT_Mez_15_44","IT_Tre_16_15",
+#                    "Machado:Linvilla:65","Machado:Linvilla:50","PT_Rec_15_16","Sedghifar:SC:Conway",
+#                    "Sedghifar:ME:Fairfield","Sedghifar:FL:Miami","Sedghifar:Panama",
+#                    "Sedghifar:NJ:Princeton","Sedghifar:RI:Providence","Sedghifar:NC:Raleigh",
+#                    "Sedghifar:VA:Richmond","Sedghifar:GA:Savannah" )
+# 
+#   colnames(datw) <- newColNames
+#   datw
+# }
+# pooledInput<- makePooledTreemix(pooledPath)
+# write.table(pooledInput,"/scratch/cat7ep/simCline/biosampleresults/treemixPooledInput.txt",row.names = F)
 
 
 ## Individual  #### 
@@ -160,9 +160,16 @@ makeIndividTreemix<- function(individPath) {
     paste(numAlt,numRef,sep=",")
   }#end popTreemix
   
+  # keep only where common with pooled
+  seqSetFilterPos(individ.gds,chr=joined_snps$chr,pos=joined_snps$pos)
   # init matrix with chromosome and position; as many rows as snps
-  treemixTable<- matrix(c(seqGetData(individ.gds,"chromosome"),seqGetData(individ.gds,"position")),
+  ## already only includes D. sim
+  treemixTable<- matrix(c(seqGetData(individ.gds,"chromosome"),seqGetData(individ.gds,"position"),
+                          seqGetData(individ.gds,"$num_allele")),
                         nrow=length(seqGetData(individ.gds,"variant.id")))
+  # filter for where biallelic only
+  treemixTable<- treemixTable[treemixTable[,3]==2,]
+  
   for(samps in popSampsList){
     treemixTable<- cbind(treemixTable,popTreemix(samps))
   }# end for
