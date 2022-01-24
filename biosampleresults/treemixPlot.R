4c8c01fd2990fe6b78e69cfd7d0def892b879de5
Courtney Tern <catern19@gmail.com>
2022-01-24 16:26:49 -0500

    add comments

--- biosampleresults/treemixPlot.R
+++ biosampleresults/treemixPlot.R
@@ -3,6 +3,7 @@
 
 # module load gcc/7.1.0  openmpi/3.1.4  R
 # R 
+### Run locally and save as 10x20 pdf
 
 
 setwd("/scratch/cat7ep/simCline/biosampleresults/treemixOutputs")
@@ -11,7 +12,7 @@ setwd("/scratch/cat7ep/simCline/biosampleresults/treemixOutputs")
 source("/scratch/cat7ep/simCline/biosampleresults/treemix-1.13/src/plotting_funcs.R")
 # source("~/Downloads/GitHub/simCline/biosampleresults/treemix-1.13/src/plotting_funcs.R")
 # run the following for m0,1,2,3
-pdf("./m0.pdf")
+pdf("./treemixPlots.pdf")
 par(adj=0,cex=0.5,mar=c(2,4,4,4),mfrow=c(2,2))
 plot_tree("simcline_treeOut_m0") 
   mtext("(A)",side=3,adj=0,cex=3)
