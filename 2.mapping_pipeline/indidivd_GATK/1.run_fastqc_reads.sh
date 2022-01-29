22c4acec9b8cf0d8d3877f319ef95446e54d2b93
Courtney Tern <catern19@gmail.com>
2022-01-29 11:27:06 -0500

    update paths; uncomment multiqc

--- 2.mapping_pipeline/indidivd_GATK/1.run_fastqc_reads.sh
+++ 2.mapping_pipeline/indidivd_GATK/1.run_fastqc_reads.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 #
-#SBATCH -J fastqcJackson # A single job name for the array
+#SBATCH -J fastqc # A single job name for the array
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem=20G
 #SBATCH --time=72:00:00
 #SBATCH --partition=standard
-#SBATCH -o /scratch/cat7ep/slurmOut/fastqcJackson.%A_%a.out # Standard output
-#SBATCH -e /scratch/cat7ep/slurmOut/fastqcJackson.%A_%a.err # Standard error
+#SBATCH -o /scratch/cat7ep/slurmOut/fastqc.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/fastqc.%A_%a.err # Standard error
 #SBATCH --account=berglandlab
 
 
-######sbatch /scratch/cat7ep/simCline/biosampleresults/1.run_fastqc_reads.sh
+######sbatch /scratch/cat7ep/simCline/2.mapping_pipeline/individ_GATK/1.run_fastqc_reads.sh
 #Load fastqc
 module load fastqc
 
 #change to folder containing reads
-cd /scratch/cat7ep/fastqJackson
+cd /scratch/cat7ep/simCline/fasterq
 
 #Make an array of all folder containing reads
 files=*
 #echo ${files}
 #set output directory
-outdir="/scratch/cat7ep/fastqc"
+outdir="/scratch/cat7ep/simCline/fasterq"
 
 #Run loop running fastqc
 for i in  ${files}
@@ -36,12 +36,11 @@ mkdir ../QC_raw_reads
 find * -name "*fastqc.zip" -exec cp {} ../QC_raw_reads \;
 
 cd ../QC_raw_reads
-#
-# =
-# #Now run multi QC
-# # I downloaded a multiQC singularity thanks to Cory.
-# module load singularity
-# #singularity pull --name multiqc.sif shub://cory-weller/singularity:multiqc
-# singularity run /scratch/cat7ep/multiqc.sif ./
-#
-# echo "completed"
+
+#Now run multi QC
+# I downloaded a multiQC singularity thanks to Cory.
+module load singularity
+#singularity pull --name multiqc.sif shub://cory-weller/singularity:multiqc
+singularity run /scratch/cat7ep/simCline/individPipeline/multiqc.sif ./
+
+echo "completed"
