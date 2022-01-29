9895c6cb512217fdd5fed88720b9f647aced823f
Courtney Tern <catern19@gmail.com>
2022-01-29 10:58:27 -0500

    rename

--- 2.mapping_pipeline/makePooledVCF.sh
+++ 2.mapping_pipeline/makePooledVCF.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#SBATCH -J makeVCF8 # A single job name for the array
+#SBATCH -J makeVCF # A single job name for the array
 #SBATCH --ntasks-per-node=20 #twenty cores
 #SBATCH -N 1
 #SBATCH -t 96:00:00 ### 96 hrs= 4 days
@@ -13,16 +13,14 @@
 module load samtools/1.10
 module load varscan/2.4.4
 
-cd /scratch/cat7ep/simCline/biosampleresults/
-####sbatch /scratch/cat7ep/simCline/biosampleresults/makeVCF.sh
+cd /scratch/cat7ep/simCline/2.mapping_pipeline/
+####sbatch /scratch/cat7ep/simCline/2.mapping_pipeline/makeVCF.sh
 
 ###Make VCF file with samtools to VarScan pipe
 samtools mpileup -f /project/berglandlab/courtney/simCline/refgenomes/combinedref.fasta \
-   -b /scratch/cat7ep/simCline/biosampleresults/inputPooledBam.txt \
+   -b /scratch/cat7ep/simCline/2.mapping_pipeline/inputPooledBam.txt \
    -q 10 -Q 15 | \
 java -jar $EBROOTVARSCAN/VarScan.v2.4.4.jar mpileup2snp \
    --output-vcf 1 \
-   --vcf-sample-list /scratch/cat7ep/simCline/biosampleresults/inputPooledSamps.txt \
-   --variants - > /project/berglandlab/courtney/simCline/pooledData.vcf
-
-# go to vcf2gds.R
+   --vcf-sample-list /scratch/cat7ep/simCline/2.mapping_pipeline/inputPooledSamps.txt \
+   --variants - > /project/berglandlab/courtney/simCline/data_files/pooledData.vcf
