50e860c82027f35bd3c49f4a4b10dc6428f20ab9
Courtney Tern <catern19@gmail.com>
2022-02-01 17:50:22 -0500

    update paths

--- 3.analysis/treemixPlot.R
+++ 3.analysis/treemixPlot.R
@@ -6,13 +6,12 @@
 ### Run locally and save as 10x20 pdf
 
 
-setwd("/scratch/cat7ep/simCline/biosampleresults/treemixOutputs")
-# setwd("~/Downloads/GitHub/simCline/biosampleresults/treemixOutputs")
+setwd("/scratch/cat7ep/simCline/data/treemixOutputs")
+# setwd("~/Downloads/GitHub/simCline/data/treemixOutputs")
 
-source("/scratch/cat7ep/simCline/biosampleresults/treemix-1.13/src/plotting_funcs.R")
-# source("~/Downloads/GitHub/simCline/biosampleresults/treemix-1.13/src/plotting_funcs.R")
+source("/scratch/cat7ep/simCline/software/treemix-1.13/src/plotting_funcs.R")
+# source("~/Downloads/GitHub/simCline/treemix-1.13/src/plotting_funcs.R")
 # run the following for m0,1,2,3
-pdf("./treemixPlots.pdf")
 par(adj=0,cex=0.5,mar=c(2,4,4,4),mfrow=c(2,2))
 plot_tree("simcline_treeOut_m0") 
   mtext("(A)",side=3,adj=0,cex=3)
@@ -22,4 +21,4 @@ plot_tree("simcline_treeOut_m2")
   mtext("(C)",side=3,adj=0,cex=3)
 plot_tree("simcline_treeOut_m3")
   mtext("(D)",side=3,adj=0,cex=3)
-dev.off()
+
