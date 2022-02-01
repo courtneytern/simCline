eaac571997c560574e5cd28a79fc5dbf2324f70f
Courtney Tern <catern19@gmail.com>
2022-02-01 17:55:09 -0500

    update paths

--- 3.analysis/f3.sh
+++ 3.analysis/f3.sh
@@ -10,7 +10,7 @@
 #SBATCH -e /scratch/cat7ep/slurmOut/f3FINAL.%A_%a.err # Standard error
 #SBATCH --array=5-12,21,22,24-32
 
-####### sbatch /scratch/cat7ep/simCline/biosampleresults/f3.sh
+####### sbatch /scratch/cat7ep/simCline/3.analysis/f3.sh
 ## This will take each of the smaller treemix files and run f3 stats on them
 ## The outputs from this file will need to be parsed through to get the A;B,C trees of interest
 
@@ -32,13 +32,11 @@ module load intel/20.0  mvapich2/2.3.3 boost/1.68.0
 # Set paths
 THREEPOP_PATH="$HOME/Software/LocalInstall/usr/local/bin/threepop"
 
-INPUT_FOLDER=/scratch/cat7ep/simCline/biosampleresults/treemixInputs
+INPUT_FOLDER=/scratch/cat7ep/simCline/data/treemixInputs
 INPUT_FILE=${INPUT_FOLDER}/treemixInput_${SLURM_ARRAY_TASK_ID}.txt.gz
 
-OUTPUT_FOLDER=/scratch/cat7ep/simCline/biosampleresults/f3_outputs
+OUTPUT_FOLDER=/scratch/cat7ep/simCline/data/f3_outputs
 OUTPUT_FILE=${OUTPUT_FOLDER}/f3_output_${SLURM_ARRAY_TASK_ID}.txt
-
-cd /scratch/cat7ep/simCline/biosampleresults
 
 # run f3 statistics and save into out path
 $THREEPOP_PATH -i $INPUT_FILE -k 500 > $OUTPUT_FILE
