45b50b8c88a3c2a6bfbd4f7d0b695b2303a56b76
Courtney Tern <catern19@gmail.com>
2022-02-09 22:30:54 -0500

    Update filter_sites.R

--- 3.analysis/filter_sites.R
+++ 3.analysis/filter_sites.R
@@ -82,8 +82,8 @@ returnRD <- function(a,r,aa,ar){
   else
     return(r)
 }
-merged[,ad:=mapply(returnAD,merged$ad,merged$rd,merged$aveAD,merged$aveRD),
-       rd:=mapply(returnRD,merged$ad,merged$rd,merged$aveAD,merged$aveRD)]
+merged[,list(ad=mapply(returnAD,merged$ad,merged$rd,merged$aveAD,merged$aveRD),
+       rd=mapply(returnRD,merged$ad,merged$rd,merged$aveAD,merged$aveRD) )]
 merged<- merged[,c(2,4,3,5,6,14,15)]
 head(merged)
 
