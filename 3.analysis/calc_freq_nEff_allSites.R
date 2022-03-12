7e84ff33fce1b3639413fec7cef19fe0cc0f2fc6
Courtney Tern <catern19@gmail.com>
2022-03-12 10:56:09 -0500

    adding progress print statements

--- 3.analysis/calc_freq_nEff_allSites.R
+++ 3.analysis/calc_freq_nEff_allSites.R
@@ -27,22 +27,25 @@ metadata<- fread("./concatenated.csv")
 # look at DEST_freeze1 if necessary for help
 calcPooled<- function(pooledVariants){
   # keep only the relevant var ids 
+  print("Starting function")
   seqSetFilter(pooled.gds,variant.id= pooledVariants)
   
   # make table with ad,rd,nflies,dp
-  adList<- seqGetData(pooled.gds, "annotation/format/AD")
-  rdList<- seqGetData(pooled.gds, "annotation/format/RD")
-  dpList<- seqGetData(pooled.gds, "annotation/format/DP")
+  adList<- seqGetData(pooled.gds, "annotation/format/AD"); print("AD done")
+  rdList<- seqGetData(pooled.gds, "annotation/format/RD"); print("RD done")
+  dpList<- seqGetData(pooled.gds, "annotation/format/DP"); print("DP done")
+  print("Making dat...")
   dat <- data.table(population=rep(seqGetData(pooled.gds, "sample.id"), dim(adList)[2]),
                     variant.id=rep(seqGetData(pooled.gds, "variant.id"), each=dim(adList)[1]),
                     ad=expand.grid(adList)$Var1,
                     rd=expand.grid(rdList)$Var1,
                     dp=expand.grid(dpList)$Var1
-  )
+  ); print("done!")
   # get nflies for each population from metadata
   nflies<- metadata[metadata$"p/i"=="P",c("identifier","numInd")]
   nflies<- distinct(nflies) #remove machado duplicates
   #merge in nflies info
+  print("Merging nflies...")
   dat2<- merge(dat,nflies,by.x="population",by.y="identifier",all.x=T)
   # calculate neff per pool per site
   dat2[,nEff:=round((2*numInd*dp)/(2*numInd + dp))]
@@ -50,4 +53,7 @@ calcPooled<- function(pooledVariants){
   # then corrected allele counts for each site across all samples
   dat2[,AD_nEff:=af_nEff*nEff]
   dat2[,RD_nEff:=(1-af_nEff)*nEff]
+  print("All done. Returning.")
+  dat2
 }# calcPooled
+pooledFreqNeff<- calcPooled(pooledVariants)
