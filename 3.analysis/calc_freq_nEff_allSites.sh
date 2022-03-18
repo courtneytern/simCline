fcef285ab910ff810815df125dec18c5007ea035
Courtney Tern <catern19@gmail.com>
2022-03-18 12:24:33 -0400

    Create calc_freq_nEff_allSites.sh

---
+++ 3.analysis/calc_freq_nEff_allSites.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=120G
+#SBATCH --time=2:00:00
+#SBATCH --partition=largemem
+#SBATCH --account=berglandlab_standard
+#SBATCH -o /scratch/cat7ep/slurmOut/individNeff.%A_%a.out # Standard output
+#SBATCH -e /scratch/cat7ep/slurmOut/individNeff.%A_%a.err # Standard error
+
+####### sbatch /scratch/cat7ep/simCline/3.analysis/calc_freq_nEff_allSites.sh
+
+module load gcc/7.1.0  openmpi/3.1.4
+module load R
+
+Rscript --vanilla /scratch/cat7ep/simCline/3.analysis/calc_freq_nEff_allSites.R
